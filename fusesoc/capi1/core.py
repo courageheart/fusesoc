5a551bb78ac2344f7ac5e152546098d873657dfb
Olof Kindgren <olof.kindgren@gmail.com>
2020-05-08 12:30:57 +0000

    Allow using parameters defined in dependencies

--- fusesoc/capi1/core.py
+++ fusesoc/capi1/core.py
@@ -180,7 +180,7 @@ class Core:
                 files += fs.file
         return files
 
-    def get_parameters(self, flags={}):
+    def get_parameters(self, flags={}, _parameters=None) :
         self._debug("Getting parameters for flags '{}'".format(str(flags)))
         parameters = {}
         for k, v in self.parameter.items():
